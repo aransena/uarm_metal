1e757cb7ca7f07ed501f22daa000da5625f7e79e
Aran Sena <Aran Sena>
2017-03-06 21:36:32 +0000

    speed debug

--- src/UARM_interface.py
+++ src/UARM_interface.py
@@ -133,7 +133,7 @@ class UARM_interface():
         rospy.loginfo("uarm_interface running")
         while True and (rospy.is_shutdown() is False):
             request = self.get_from_interface_queue()
-            # print self.uarm_interface_queue.qsize()
+            print self.uarm_interface_queue.qsize()
             if request == "READ":
                 curr_vals = self.read_position()
                 try:
