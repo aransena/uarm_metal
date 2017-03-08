caa15107a992b36e155230243ff78247cf8d4390
Aran Sena <Aran Sena>
2017-03-08 22:37:39 +0000

    rosification

---
+++ examples/ROS_record_play.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+import signal
+import time
+import rospy
+from std_msgs.msg import String, Bool
+from uarm_metal.msg import JointAngles, Beep
+
+def shutdown_signal_handler(signal, frame):
+    global exit
+    exit = True
+    print 'Shutting Down!'
+
+def data_callback(data):
+    #print data
+    pass
+
+def process(key):
+    print key
+    if key == '1':
+        pass
+    elif key == '2':
+        pass
+    elif key == '3':
+        pass
+    elif key == '4':
+        pass
+
+
+if __name__ == '__main__':
+    signal.signal(signal.SIGINT, shutdown_signal_handler)
+    print 'Ctrl+C to exit'
+    exit = False
+
+    ja_pub = rospy.Publisher('/uarm_metal/joint_angles_write', JointAngles, queue_size=10)
+    att_put = rospy.Publisher('/uarm_metal/attach', Bool, queue_size=10)
+    beep_pub = rospy.Publisher('/uarm_metal/beep', Beep, queue_size=10)
+
+    rospy.Subscriber("/uarm_metal/joint_angles_read", JointAngles, data_callback)
+    rospy.init_node('record_play_node', anonymous=True)
+
+    rw_rate = 1000.0
+    rec_data = []
+    play_data = []
+    start = True
+    while exit is False:
+
+        key = getch()
+        process(key)
+        time.sleep(1.0 / rw_rate)
+
