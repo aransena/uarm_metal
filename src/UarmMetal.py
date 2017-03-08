b62339f1db2f7d0c42a1eaed05c6c2433ec6c896
Aran Sena <Aran Sena>
2017-03-08 20:45:50 +0000

    rosification

--- src/UarmMetal.py
+++ src/UarmMetal.py
@@ -11,7 +11,7 @@ from std_msgs.msg import String, Bool
 from std_msgs.msg import Float32MultiArray as Array
 from std_msgs.msg import MultiArrayDimension as ArrayDims
 
-from msg import Position, JointAngles
+from uarm_metal.msg import Position, JointAngles
 
 
 class UarmMetal():
