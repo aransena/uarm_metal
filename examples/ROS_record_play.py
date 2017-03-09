9e2f7071455d237b41e8c4b3a5a45eaf49e5f775
Aran Sena <Aran Sena>
2017-03-09 19:33:54 +0000

    rec play

--- examples/ROS_record_play.py
+++ examples/ROS_record_play.py
@@ -43,7 +43,7 @@ def start_playback(data):
 def play(*args):
     global publishers
     global beeps
-    rate = rospy.Rate(40)
+    rate = rospy.Rate(30)
     for point in args:
         publishers['data'].publish(point)
         rate.sleep()
