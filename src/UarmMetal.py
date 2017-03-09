3c4cfca6684d0c9abab6d86b55333905b322844c
Aran Sena <Aran Sena>
2017-03-09 20:36:11 +0000

    rec play

--- src/UarmMetal.py
+++ src/UarmMetal.py
@@ -352,8 +352,8 @@ class UarmMetal():
         self.iq.send_to_queue(msg)
 
     def request_ja(self, data):
-        msg = "JA" + str(round(data.j0,2)) + "," + str(round(data.j1,2)) \
-              + "," + str(round(data.j2,2)) + "," + str(round(data.j3,2))
+        msg = "JA" + str(round(data.j0,0)) + "," + str(round(data.j1,0)) \
+              + "," + str(round(data.j2,0)) + "," + str(round(data.j3,0))
         print msg, map(float, msg[2:].split(','))
         self.iq.send_to_queue(msg)
 
